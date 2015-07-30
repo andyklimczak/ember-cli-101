eedc6918ebe9a0c4239e3e603f690a35bd3f3e87
Andy Klimczak <andyklimczak@fastmail.com>
2015-07-30 22:08:12 +0000

    pre working with javascript plugins

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,21 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
   },
+  activate() {
+    console.log('----- activate hook called -----');
+  },
+  deactivate() {
+    console.log('----- deactivate hook called -----');
+  },
+  resetController: function(controller, isExiting, transition) {
+    if (isExiting) {
+      var model = controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  },
   actions: {
     save() {
       console.log('+-- save action bubbled up to friends new route');
